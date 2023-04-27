065ba25ab30e335a3b9fee19fafc22ffc9dbbeb2
mariya-domakova <mashadom46@gmail.com>
2023-04-27 04:05:50 +0300

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+        int grade = int.Parse(valueFromUser);
+
+        string letter = "";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you haven't passed the course. Try next time!");  
+        }
     }
 }
