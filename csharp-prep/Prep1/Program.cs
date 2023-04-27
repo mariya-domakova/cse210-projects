4132c354eeffb4a49f94a5c7b2288b2e6297e2e5
mariya-domakova <mashadom46@gmail.com>
2023-04-27 03:44:48 +0300

    Changed variables names

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {first_name}, {first_name} {last_name}.");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstName}, {firstName} {lastName}.");
     }
 }
