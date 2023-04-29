46e2c4e537498045516b1f9307fd049972600508
mariya-domakova <122839487+mariya-domakova@users.noreply.github.com>
2023-04-30 00:04:16 +0300

    Finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+            number = int.Parse(userResponse);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;    
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = 0;
+        largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
